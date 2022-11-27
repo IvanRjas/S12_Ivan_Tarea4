db9b46a7230512f74de73084008fc4660cf66f20
IvanRjas <118936734+IvanRjas@users.noreply.github.com>
2022-11-27 12:33:09 -0600

    Subiendo hola mundo.c

---
+++ Hola mundo.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+Autor: Iván Rojas Calvo
+Curso: Aplicaciones Microcontroladores
+Actividad: Creación y modificación de repositorio con Git Bash y Github
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hola mundo");
+
+    return 0;
+}
